76d08d8c59401da54b1156c9f2c9ebef3b78928a
njuwzt <985235380@qq.com>
2020-11-12 17:49:10 +0800

    update

--- homework4/shopping/shopping/ViewController.swift
+++ homework4/shopping/shopping/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     //MASK:Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var goodsNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -28,6 +29,15 @@ class ViewController: UIViewController,UITextFieldDelegate {
         goodsNameLabel.text=textField.text
     }
     //MASK:Action
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        //imagePickerController.sourceType = .photoLibrary
+        imagePickerController.sourceType = .camera
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         goodsNameLabel.text = "Default Text"
     }
